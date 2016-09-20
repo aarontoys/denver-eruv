31897c42fa6243eee355d13aff7c6470207b67f7
Aaron Toys <aarontoys@gmail.com>
2016-09-20 14:00:43 -0600

    add map to new map tab

--- myApp/www/app/map/map.controllers.js
+++ myApp/www/app/map/map.controllers.js
@@ -4,10 +4,24 @@
   .module('starter')
   .controller('mapCtrl', mapCtrl);
 
+  mapCtrl.$inject = [];
+
   function mapCtrl () {
     var vm = this;
 
     vm.test = 'hello world';
+
+    initMap();
+
+    function initMap () {
+      // Create a map object and specify the DOM element for display.
+      console.log('working?');
+      var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 39.712653, lng: -104.902180},
+        scrollwheel: false,
+        zoom: 14
+      });
+    }
   }
 
 })();
