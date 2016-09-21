d46315c42a17f397a7d4ed6009b92e68f39ff18c
Aaron Toys <aarontoys@gmail.com>
2016-09-21 11:05:39 -0600

    fix CORS issue by allowing CORS

--- src/server/app.js
+++ src/server/app.js
@@ -33,6 +33,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client')));
 
+// *** allow CORS *** //
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // *** main routes *** //
 app.use('/', routes);
