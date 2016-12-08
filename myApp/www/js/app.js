489ab241081974dd4596c6a3cc85220bb59771a4
Aaron Toys <aarontoys@gmail.com>
2016-12-08 12:38:40 -0700

    update UI Router states and configure new view, also renamed other tabs

--- myApp/www/js/app.js
+++ myApp/www/js/app.js
@@ -50,24 +50,34 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     }
   })
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
+  // .state('tab.chats', {
+  //     url: '/chats',
+  //     views: {
+  //       'tab-chats': {
+  //         templateUrl: 'templates/tab-chats.html',
+  //         controller: 'ChatsCtrl'
+  //       }
+  //     }
+  //   })
+  //   .state('tab.chat-detail', {
+  //     url: '/chats/:chatId',
+  //     views: {
+  //       'tab-chats': {
+  //         templateUrl: 'templates/chat-detail.html',
+  //         controller: 'ChatDetailCtrl'
+  //       }
+  //     }
+  //   })
+  .state('tab.reportView', {
+    url: '/reportView',
+    views: {
+      'tab-reportView': {
+        templateUrl: 'app/reportView/reportView.view.html',
+        controller: 'reportViewCtrl',
+        controllerAs: 'vm'
       }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+    }
+  })
 
   .state('tab.reportForm', {
     url: '/reportForm',
