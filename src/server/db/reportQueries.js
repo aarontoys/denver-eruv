4a8a088ec3e7c60d8fef5c5a2bb0db8e32164575
Aaron Toys <aarontoys@gmail.com>
2017-02-09 21:25:36 -0700

    update join query to explicitly select columns

--- src/server/db/reportQueries.js
+++ src/server/db/reportQueries.js
@@ -4,7 +4,11 @@ var now = new Date();
 var Report = function () {
   return knex('report_log')
           .join('issues', 'report_log.issue_id', '=', 'issues.id')
-          .join('severities', 'report_log.severity_id', '=', 'severities.id');
+          .join('severities', 'report_log.severity_id', '=', 'severities.id')
+          .select(report_log.id,report_log.user_id,report_log.issue_id,report_log.severity_id,
+            report_log.address,report_log.lat,report_log.lon,report_log.img,report_log.other_issue,
+            report_log.other_severity,report_log.created_at,report_log.updated_at,severities.severity,
+            issues.issue);
 };
 
 function addLog (userId, issueId, severityId, address, lat, lon, img) {
