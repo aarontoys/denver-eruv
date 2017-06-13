86a72bdce7e10814bb2fcc1daad0543511882d58
Aaron Toys <aarontoys@gmail.com>
2017-06-12 22:27:27 -0600

    more manual resetting

--- myApp/www/app/reportForm/reportForm.controllers.js
+++ myApp/www/app/reportForm/reportForm.controllers.js
@@ -4,7 +4,7 @@
   .module('starter')
   .controller('reportFormCtrl', reportFormCtrl);
 
-  reportFormCtrl.$inject = ['mapService', 'logService', '$cordovaCamera', '$cordovaToast']
+  reportFormCtrl.$inject = ['mapService', 'logService', '$cordovaCamera', '$cordovaToast'];
 
   function reportFormCtrl (mapService, logService, $cordovaCamera, $cordovaToast) {
     var vm = this;
@@ -37,7 +37,7 @@
 
           if (vm.imgLen > 100000) {
 
-            $cordovaToast.showShortCenter('Image was too large, please try again . . .')
+            $cordovaToast.showShortCenter('Image was too large, please try again . . .');
             var factor = Math.sqrt(1 - (vm.imgLen - 90000)/vm.imgLen);
             options.targetWidth = Math.round(options.targetWidth * factor);
             options.targetHeight = Math.round(options.targetHeight * factor);
@@ -52,14 +52,14 @@
         });
 
       } else {
-        vm.message = 'The ability to take a photo requires a mobile deveice'
+        vm.message = 'The ability to take a photo requires a mobile deveice';
       }
 
     };
 
 
     vm.submit = function (issue, severity) {
-      logService.createLogItem (2,issue,severity,vm.address.address,vm.position,vm.img,1,vm.comments, vm.bucket_truck)
+      logService.createLogItem (1,issue,severity,vm.address.address,vm.position,vm.img,1,vm.comments, vm.bucket_truck)
       .then(function (result) {
         if (result[0]) {
           vm.success = 'Successful! Id = ' + result[0];
@@ -69,11 +69,11 @@
             options.targetHeight = 756;
           }
         } else {
-          vm.success = 'Image is too large. Please try again'
+          vm.success = 'Image is too large. Please try again';
         }
       })
       .catch (function (err) {
-        vm.success = 'Error: ', err;
+        vm.success = 'Error: ' + err;
         return err;
       });
     };
@@ -88,7 +88,7 @@
     .catch(function (err) {
       console.log(err);
       return err;
-    })
+    });
 
   }
 
